697fc36b26a9e2548aa946d5e1bae67737092b25
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2024-01-17 03:43:41 +0800

    [update]: separate readme from pypi description
    remove version.txt
    
    ignore direct link requirement if installed from pypi

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ from setuptools import find_packages, setup
 import os
 
 def fetch_readme():
-    with open('README.md', encoding='utf-8') as f:
+    with open('README-pypi.md', encoding='utf-8') as f:
         text = f.read()
     return text
 
 def fetch_requirements():
     filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
     with open(filename, 'r') as f:
-        envs = [line.rstrip('\n') for line in f.readlines()]
+        envs = [line.rstrip('\n') for line in f.readlines() if '@' not in line]
     return envs
 
-version = open('version.txt', 'r').read().rstrip('\n')
 install_requires = fetch_requirements()
 setup(name='vbench',
       version='0.1.0',
