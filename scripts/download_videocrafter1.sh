627d2d7e2f0829e79cbdd86392e1a32f87bd3dab
zhangfan-p <zhangfan199903@outlook.com>
2024-04-01 23:09:17 +0800

    [add] example of evaluation

---
+++ scripts/download_videocrafter1.sh
@@ -0,0 +1,4 @@
+mkdir -p sampled_videos
+gdown --id 1FCRj48-Yv7LM7XGgfDCvIo7Kb9EId5KX --output sampled_videos/videocrafter-1.tar.gz
+tar -xvf sampled_videos/videocrafter-1.tar.gz -C sampled_videos
+rm -f sampled_videos/videocrafter-1.tar.gz
