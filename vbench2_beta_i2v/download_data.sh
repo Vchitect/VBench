0b3cbf7f2c4355e128022e35e257f3d6d919e1a1
zhangfan-p <zhangfan199903@outlook.com>
2024-04-26 11:39:43 +0800

    [update] download script

--- vbench2_beta_i2v/download_data.sh
+++ vbench2_beta_i2v/download_data.sh
@@ -1,5 +1,8 @@
 mkdir -p vbench2_beta_i2v/data
 gdown --id 1zmWs_m_A4q6YgTZwIZ230jW0ttknlGJA --output vbench2_beta_i2v/data/i2v-bench-info.json
 gdown --id 1JANXpTxg90M3Exi5WGnVNagb1nqyTJ4o --output vbench2_beta_i2v/data/crop.zip
+gdown --id 1qhkLCSBkzll0dkKpwlDTwLL0nxdQ4nrY --output vbench2_beta_i2v/data/origin.zip
 unzip vbench2_beta_i2v/data/crop.zip -d vbench2_beta_i2v/data
+unzip vbench2_beta_i2v/data/origin.zip -d vbench2_beta_i2v/data
 rm -f vbench2_beta_i2v/data/crop.zip
+rm -f vbench2_beta_i2v/data/origin.zip
