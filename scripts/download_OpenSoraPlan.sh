1569bd0bc5849baafacc9b98d85cad02f57d19d1
MqLeet <mqleet@gmail.com>
2024-07-27 21:07:42 +0800

    [add] Example of Evaluating OpenSoraPlan

---
+++ scripts/download_OpenSoraPlan.sh
@@ -0,0 +1,4 @@
+mkdir -p sampled_videos
+gdown --id 1zsg-HPiqYZJoryTXw6cG_9deBlIbZN87 --output sampled_videos/OpenSoraPlanv1-1.tar
+tar -xvf sampled_videos/OpenSoraPlanv1-1.tar -C sampled_videos
+rm -f sampled_videos/OpenSoraPlanv1-1.tar
---
+++ scripts/evaluate_OpenSoraPlan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the dimension list
+dimensions=("subject_consistency" "background_consistency" "aesthetic_quality" "imaging_quality" "object_class" "multiple_objects" "color" "spatial_relationship" "scene" "temporal_style" "overall_consistency" "human_action" "temporal_flickering" "motion_smoothness" "dynamic_degree" "appearance_style")
+
+# Corresponding folder names
+folders=("subject_consistency" "scene" "overall_consistency" "overall_consistency" "object_class" "multiple_objects" "color" "spatial_relationship" "scene" "temporal_style" "overall_consistency" "human_action" "temporal_flickering" "subject_consistency" "subject_consistency" "appearance_style")
+
+
+
+# Base path for videos
+base_path='./sampled_videos/OpenSoraPlanv1-1'  # TODO: change to local path
+
+# Loop over each dimension
+for i in "${!dimensions[@]}"; do
+    # Get the dimension and corresponding folder
+    dimension=${dimensions[i]}
+    folder=${folders[i]}
+
+    # Construct the video path
+    videos_path="${base_path}/${folder}"
+    echo "$dimension $videos_path"
+
+    # Run the evaluation script
+    python vbench2_beta_long/eval_long.py --videos_path $videos_path --dimension $dimension --mode 'long_vbench_standard' --dev_flag
+done
