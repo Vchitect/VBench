86e060a24e0e808e58df9d7f15b207a156137227
NattapolChan <nattapolchanpaisit@gmail.com>
2024-08-23 14:53:00 +0800

    fix: remove --custom_input in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -57,12 +57,6 @@ def parse_args():
         """,
     )
     parser.add_argument(
-        "--custom_input",
-        action="store_true",
-        required=False,
-        help="(deprecated) use --mode=\"custom_input\" instead",
-    )
-    parser.add_argument(
         "--prompt",
         type=str,
         default="",
@@ -79,7 +73,7 @@ def parse_args():
         help="""Specify the path of the file that contains prompt lists
         If not specified, filenames will be used as input prompts
         * Mutually exclusive to --prompt.
-        ** This option must be used with --custom_input flag
+        ** This option must be used with --mode=custom_input flag
         """
     )
     parser.add_argument(
