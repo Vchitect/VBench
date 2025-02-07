33153f8049615eaac9ef164a4ab224ea8a328f5f
zhangfan-p <zhangfan199903@outlook.com>
2025-02-07 22:24:17 +0800

    [update] automatic evaluation script

--- vbench2_beta_long/evaluate_long.sh
+++ vbench2_beta_long/evaluate_long.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
 
+base_path="$1"
+
 # Define the dimension list
 dimensions=("subject_consistency" "background_consistency" "aesthetic_quality" "imaging_quality" "object_class" "multiple_objects" "color" "spatial_relationship" "scene" "temporal_style" "overall_consistency" "human_action" "temporal_flickering" "motion_smoothness" "dynamic_degree" "appearance_style")
 
 # Corresponding folder names
 folders=("subject_consistency" "scene" "overall_consistency" "overall_consistency" "object_class" "multiple_objects" "color" "spatial_relationship" "scene" "temporal_style" "overall_consistency" "human_action" "temporal_flickering" "subject_consistency" "subject_consistency" "appearance_style")
 
-# Base path for videos
-base_path='./sampled_long_videos'  # TODO: change to local path
+# Check if the necessary subdirectories exist in the base path
+subdirs_found=false
 
-# Loop over each dimension
-for i in "${!dimensions[@]}"; do
-    # Get the dimension and corresponding folder
-    dimension=${dimensions[i]}
-    folder=${folders[i]}
-
-    # Construct the video path
-    videos_path="${base_path}/${folder}"
-    echo "$dimension $videos_path"
-
-    # Run the evaluation script
-    if [ "$dimension" == "temporal_flickering" ]; then
-        python vbench2_beta_long/eval_long.py --videos_path $videos_path --dimension $dimension --mode 'long_vbench_standard' --dev_flag --static_filter_flag
-    else
-        python vbench2_beta_long/eval_long.py --videos_path $videos_path --dimension $dimension --mode 'long_vbench_standard' --dev_flag
+for folder in "${folders[@]}"; do
+    if [ -d "$base_path/$folder" ]; then
+        subdirs_found=true
+        break
     fi
 done
+
+# If subdirectories are found, evaluate them, otherwise use base_path
+if [ "$subdirs_found" = true ]; then
+    # Loop over each dimension and corresponding folder
+    for i in "${!dimensions[@]}"; do
+        dimension=${dimensions[i]}
+        folder=${folders[i]}
+
+        videos_path="${base_path}/${folder}"
+        echo "Evaluating '$dimension' in $videos_path"
+
+        # Check if the dimension is 'temporal_flickering' and add the static filter flag
+        if [ "$dimension" == "temporal_flickering" ]; then
+            python vbench2_beta_long/eval_long.py --videos_path $videos_path --dimension $dimension --mode 'long_vbench_standard' --dev_flag --static_filter_flag
+        else
+            python vbench2_beta_long/eval_long.py --videos_path $videos_path --dimension $dimension --mode 'long_vbench_standard' --dev_flag
+        fi
+    done
+else
+    # If no subdirectories are found, set videos_path to base_path
+    videos_path="$base_path"
+    echo "No subdirectories found. Using base path $videos_path for evaluation."
+
+    # Run the evaluation
+    for i in "${!dimensions[@]}"; do
+        dimension=${dimensions[i]}
+        echo "Evaluating '$dimension' in $videos_path"
+
+        # Check if the dimension is 'temporal_flickering' and add the static filter flag
+        if [ "$dimension" == "temporal_flickering" ]; then
+            python vbench2_beta_long/eval_long.py --videos_path $videos_path --dimension $dimension --mode 'long_vbench_standard' --dev_flag --static_filter_flag
+        else
+            python vbench2_beta_long/eval_long.py --videos_path $videos_path --dimension $dimension --mode 'long_vbench_standard' --dev_flag
+        fi
+    done
+fi
