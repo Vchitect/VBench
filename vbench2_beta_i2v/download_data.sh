6446112aa373b20602556ead2b63aa282b849b3e
zhangfan-p <zhangfan199903@outlook.com>
2025-03-06 21:07:38 +0800

    [add] ratio 3-2

--- vbench2_beta_i2v/download_data.sh
+++ vbench2_beta_i2v/download_data.sh
@@ -1,6 +1,6 @@
 mkdir -p vbench2_beta_i2v/data
 gdown --id 1zmWs_m_A4q6YgTZwIZ230jW0ttknlGJA --output vbench2_beta_i2v/data/i2v-bench-info.json
-gdown --id 1JANXpTxg90M3Exi5WGnVNagb1nqyTJ4o --output vbench2_beta_i2v/data/crop.zip
+gdown --id 1Y_JnYnyJ3a6QhiranoX0MQVZFcTDPekZ --output vbench2_beta_i2v/data/crop.zip
 gdown --id 1qhkLCSBkzll0dkKpwlDTwLL0nxdQ4nrY --output vbench2_beta_i2v/data/origin.zip
 unzip vbench2_beta_i2v/data/crop.zip -d vbench2_beta_i2v/data
 unzip vbench2_beta_i2v/data/origin.zip -d vbench2_beta_i2v/data
