5d8113dda8a0005432801952296d28f925bc8a1a
zou <sowhata@vip.qq.com>
2025-04-01 20:09:17 +0800

    UPDATE

--- VBench-2.0/vbench2/third_party/ViTDetector/run_train.sh
+++ VBench-2.0/vbench2/third_party/ViTDetector/run_train.sh
@@ -1,1 +1,4 @@
+# download pretrain checkpoint at  https://drive.google.com/file/d/1dJn6GYkwMIcoP3zqOEyW1_iQfpBi8UOw/view?usp=sharing
+
+
 torchrun main_finetune.py --cfg 'configs/vit_base__800ep/simmim_finetune__vit_base__img224__800ep.yaml' --train-path "path-to-train-set-json" --val-path "path-to-val-set-json" --pretrained 'path-to-pretrained-vit-base' --batch-size 128 --output "output-path"
