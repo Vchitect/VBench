d9dfbc8042f45d8077e6c89affb21d998d2c5fcb
zhengdian <1423606603@qq.com>
2025-04-15 02:05:57 +0800

    [update] VBench-2.0 Submission, PYPI, FAQ

---
+++ VBench-2.0/vbench2/cli/evaluate.py
@@ -0,0 +1,117 @@
+import os
+import shlex
+import subprocess
+import argparse
+
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+def register_subparsers(subparser):
+    parser = subparser.add_parser('evaluate', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(
+        "--ngpus",
+        type=int,
+        default=1,
+        help="Number of GPUs to run evaluation on"
+        )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default='./evaluation_results/',
+        help="output path to save the evaluation results",
+    )
+    parser.add_argument(
+        "--full_json_dir",
+        type=str,
+        default=f'{CUR_DIR}/../VBench2_full_info.json',
+        help="path to save the json file that contains the prompt and dimension information",
+    )
+    parser.add_argument(
+        "--videos_path",
+        type=str,
+        required=True,
+        help="folder that contains the sampled videos",
+    )
+    parser.add_argument(
+        "--dimension",
+        type=str,
+        required=True,
+        help="list of evaluation dimensions, usage: --dimension <dim_1> <dim_2>",
+    )
+    parser.add_argument(
+        "--load_ckpt_from_local",
+        type=bool,
+        required=False,
+        help="whether load checkpoints from local default paths (assuming you have downloaded the checkpoints locally",
+    )
+    parser.add_argument(
+        "--read_frame",
+        type=bool,
+        required=False,
+        help="whether directly read frames, or directly read videos",
+    )
+    parser.add_argument(
+        "--mode",
+        choices=['vbench_standard'],
+        default='vbench_standard',
+        help="""This flags determine the mode of evaluations, choose one of the following:
+        1. "vbench_standard": evaluate on standard prompt suite of VBench
+        """,
+    )
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        default=None,
+        help="""Specify the input prompt
+        If not specified, filenames will be used as input prompts
+        * Mutually exclusive to --prompt_file.
+        ** This option must be used with --mode=custom_input flag
+        """
+    )
+    parser.add_argument(
+        "--prompt_file",
+        type=str,
+        required=False,
+        help="""Specify the path of the file that contains prompt lists
+        If not specified, filenames will be used as input prompts
+        * Mutually exclusive to --prompt.
+        ** This option must be used with --mode=custom_input flag
+        """
+    )
+
+    ## for dimension specific params ###
+    parser.add_argument(
+        "--imaging_quality_preprocessing_mode",
+        type=str,
+        required=False,
+        default='longer',
+        help="""This is for setting preprocessing in imaging_quality
+        1. 'shorter': if the shorter side is more than 512, the image is resized so that the shorter side is 512.
+        2. 'longer': if the longer side is more than 512, the image is resized so that the longer side is 512.
+        3. 'shorter_centercrop': if the shorter side is more than 512, the image is resized so that the shorter side is 512. 
+        Then the center 512 x 512 after resized is used for evaluation.
+        4. 'None': no preprocessing
+        """,
+    )
+    parser.set_defaults(func=evaluate)
+
+def stringify_cmd(cmd_ls):
+    cmd = ""
+    for string in cmd_ls:
+        cmd += string + " "
+    return cmd
+
+## TODO
+def evaluate(args):
+    args_dict = vars(args)
+    abs_path_main = os.path.abspath(f'{CUR_DIR}/../launch/evaluate.py')
+    abs_path_json = os.path.abspath(f'{CUR_DIR}/../VBench2_full_info.json')
+    cmd = [
+        'python3',
+        abs_path_main,
+        '--output_path', os.path.join(args_dict['output_path'], args_dict['dimension']),
+        '--full_json_dir', abs_path_json,
+        '--videos_path', args_dict['videos_path'],
+        '--dimension', args_dict['dimension'],
+        '--mode', args_dict['mode'],
+        '--imaging_quality_preprocessing_mode', 'longer'
+    ]
+    subprocess.run(cmd)
